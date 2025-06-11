4b817e7da65e2b7d4b4d084310d52f06e6549115
Georgios Chliavoras <gah1960@gmail.com>
2025-06-11 21:59:53 +0200

    Search icon modification, for reusability with different themes

--- src/components/ExperiencesPage/Search.tsx
+++ src/components/ExperiencesPage/Search.tsx
@@ -2,6 +2,7 @@
 
 import { useFilter } from "@/context/FilterContext";
 import Image from "next/image";
+import { SearchIcon } from "../shared/IconComponents";
 
 const SearchInput = () => {
   const { searchQuery, setSearchQuery } = useFilter();
@@ -12,20 +13,15 @@ const SearchInput = () => {
 
   return (
     <div className="bg-yellow-400 py-10 px-4 flex justify-center">
-      <div className="flex items-center space-x-4 bg-white rounded-full px-4 py-2 shadow-md w-full max-w-xl">
+      <div className="flex items-center space-x-4 bg-white text-[#FF2AAA] rounded-full px-4 py-2 shadow-md w-full max-w-xl">
  
-        <Image 
-        src="search.svg" alt='search' className='w-6 h-6' 
-        layout="intrinsic" 
-        width={100} 
-        height={50}
-        />
+        <SearchIcon className="w-6 h-6" />
         <input
           type="text"
           value={searchQuery}
           onChange={handleInputChange}
           placeholder="Search with AI! Just tell us what you want!"
-          className="w-full bg-transparent text-xs text-[#FF2AAA] placeholder-[#FF2AAA]-500 focus:outline-none sm:text-lg"
+          className="w-full bg-transparent text-xs focus:outline-none sm:text-lg"
         />
       </div>
     </div>
